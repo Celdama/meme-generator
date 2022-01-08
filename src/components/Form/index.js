109f34c0564e546a029c2c334dcf9927f69ca3ae
Celdama <celdama.dev@gmail.com>
2022-01-08 10:16:15 +0100

    src: added function to get random img url

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Wrapper, Content, Button } from './Form.styles';
+import memesData from '../../memesData';
 
 const Form = () => {
+  const getMemeImg = (event) => {
+    event.preventDefault();
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const { url } = memesArray[randomNumber];
+    console.log(url);
+  };
+
   return (
     <Wrapper>
       <Content>
@@ -10,7 +19,7 @@ const Form = () => {
             <input placeholder='Top text' type='text' />
             <input placeholder='Bottom text' type='text' />
           </div>
-          <Button>Get a new meme image</Button>
+          <Button onClick={getMemeImg}>Get a new meme image</Button>
         </form>
       </Content>
     </Wrapper>
