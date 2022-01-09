d23994eb0734dc31e46b5adf6b218621734e0b14
Celdama <celdama.dev@gmail.com>
2022-01-09 09:32:53 +0100

    components/Ternary/index: handle isGoingOut in state

--- src/components/Ternary/index.js
+++ src/components/Ternary/index.js
@@ -1,10 +1,10 @@
 // composant inutile pour mon app
 // juste pour pratiquer the ternary operator
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const Ternary = () => {
-  const isGoingOut = false;
+  const [isGoingOut, setIsGoingOut] = useState(false);
 
   // if (isGoingOut === true) {
   //   answer = 'yes';
@@ -15,12 +15,16 @@ const Ternary = () => {
   // ternary outside JSX
   // let answer = isGoingOut ? 'yes' : 'no';
 
+  const toggleIsGoingOut = () => {
+    setIsGoingOut((prevState) => !prevState);
+  };
+
   return (
     <div>
       <h1>Do i feel like going out tonigh ?</h1>
       <div>
         {/* ternary inside JSX, no need extra variable */}
-        <h1>{isGoingOut ? 'Yes' : 'No'}</h1>
+        <h1 onClick={toggleIsGoingOut}>{isGoingOut ? 'Yes' : 'No'}</h1>
       </div>
     </div>
   );
