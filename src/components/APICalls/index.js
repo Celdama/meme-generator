bd8743d294c46fba6f7ede66d2b8002d88840434
Celdama <celdama.dev@gmail.com>
2022-01-11 10:16:35 +0100

    components/APICalls/index: Add quizz about useEffect function

--- src/components/APICalls/index.js
+++ src/components/APICalls/index.js
@@ -1,5 +1,5 @@
 // composant inutile pour l'app, juste là pour teste des call API
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const API = () => {
   const [starWarsData, setStarWarsData] = useState({});
@@ -9,9 +9,13 @@ const API = () => {
 
   console.log('component rendered');
 
-  fetch('https://swapi.dev/api/people/1')
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  // side effects
+  useEffect(() => {
+    console.log('effect');
+
+    fetch('https://swapi.dev/api/people/1').then((res) => res.json());
+    // .then((data) => setStarWarsData(data));
+  });
 
   return (
     <div>
@@ -34,3 +38,23 @@ export default API;
 //    ~ subscriptions (e.g web sockets)
 //    ~ syncing 2 different internal states together
 //    ~ basically anything that React isn't in charge of
+
+// QUIZZ useEffect //
+// 1. What is a "side effect" in React? What are some examples?
+// side effect = everything take place outside the react app
+// - Any code that affects an outside system.
+// - local storage, API, websockets, two states to keep in sync
+
+// 2. What is NOT a "side effect" in React? Examples?
+// Anything that React is in charge off
+// Maintaining state, keeping the UI in sync with the data, render DOM elements
+
+// 3. When does React run your useEffect function? When does it NOT run the effect function?
+// As soon as the component load (first render)
+// On every re-render of the component (assuming no dependencies array)
+// Will NOT run the effect when the values of the dependencies in the array stay the same between renders
+
+// 4. How would you explain what the "dependecies array" is?
+// a tool to control the run of our useEffect function.
+
+// END QUIZZ //
