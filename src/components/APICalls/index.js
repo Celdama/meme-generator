277e333db10b6b545fc61f87e66a32b08544e7e7
Celdama <celdama.dev@gmail.com>
2022-01-11 10:17:05 +0100

    components/APICalls/index: complete quizz

--- src/components/APICalls/index.js
+++ src/components/APICalls/index.js
@@ -46,15 +46,16 @@ export default API;
 // - local storage, API, websockets, two states to keep in sync
 
 // 2. What is NOT a "side effect" in React? Examples?
-// Anything that React is in charge off
-// Maintaining state, keeping the UI in sync with the data, render DOM elements
+// - Anything that React is in charge off
+// - Maintaining state, keeping the UI in sync with the data, render DOM elements
 
 // 3. When does React run your useEffect function? When does it NOT run the effect function?
-// As soon as the component load (first render)
-// On every re-render of the component (assuming no dependencies array)
-// Will NOT run the effect when the values of the dependencies in the array stay the same between renders
+// - As soon as the component load (first render)
+// - On every re-render of the component (assuming no dependencies array)
+// - Will NOT run the effect when the values of the dependencies in the array stay the same between renders
 
 // 4. How would you explain what the "dependecies array" is?
-// a tool to control the run of our useEffect function.
+// - Second parameter to the useEffect function
+// - A way for React to know whether it should to re-run the effect function
 
 // END QUIZZ //
